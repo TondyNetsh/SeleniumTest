a5cbac12a15aa8bc3defa54b1b5cf4aceb2b0a68
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2022-05-11 11:55:19 +0200

    Selenium Test

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -1,8 +1,15 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class StepDefinitions {
+    WebDriver driver = new ChromeDriver();
+
+    driver.
+
     @Given("^User is on google homepage$")
     public void user_is_on_google_homepage() throws Throwable {
         System.out.println("User is on google homepage");
